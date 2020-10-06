6fc65db76e4fdf3c1c657b76dc8b246de6d2a996
Detached <40892837+DetachedOnu@users.noreply.github.com>
2020-10-06 16:23:25 +0530

    Add files via upload

---
+++ Master_View_Final_Master_Marketing_DashBoard.py
@@ -0,0 +1,442 @@
+#!/usr/bin/env pythn
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[36]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
+#Simple python script for creating a table in postgreSQL
+
+import pandas as pd
+from pandas import DataFrame
+import os as os
+import json
+import gspread
+import oauth2client
+import csv
+import datetime
+import pymysql
+from psycopg2 import IntegrityError
+import psycopg2
+from oauth2client.service_account import ServiceAccountCredentials
+
+#os.chdir(r"C:\Users\Admin\Desktop\My Tree\BackUp_Data")
+
+conn2 = psycopg2.connect(user = "workindiauserread",
+                         password = "EBkaGnz2xGn5kQHk",
+                         host = "workindia-redshift-cluster-v2.c4n7phkfcstn.us-east-1.redshift.amazonaws.com",
+                         port = "5439",
+                         database = "workindia"
+                        )
+x1 = conn2.cursor()
+
+sql1 = """
+-- the Master Query
+-- Need to Add :
+/*
+Select 'Product_Growth_Job_Posts' as Data_cut, jp.city as City_, "Today"  as Data_As_Of ,date(ed.created_at) as Created_at,
+count(distinct(jp.job_post_id)) as Value
+from api_jobpost jp
+join api_employerdetail as ed on jp.user_id = ed.user_id
+where jp.user_id in (select distinct wp.employer_detail_id from api_employerdetailwalletpassbook wp
+where wp.transaction_source like '%recipient_wal_share_earn_1000%'
+and date(wp.created_at) between @Start and @End	)
+*/
+
+select 'Overall_Job_Posts' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(jp.timestamp) as Created_at,
+count(distinct(jp.job_post_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'FB_Job_Posts' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(jp.timestamp) as Created_at,
+count(distinct(jp.job_post_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (referrer like '%fb %' OR referrer like '%=fb%' OR referrer like '%=Fb%')
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'Google_Job_Posts' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(jp.timestamp) as Created_at,
+count(distinct(jp.job_post_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (referrer like '%SEM%' OR referrer like '%gclid%' OR referrer like '%Google%' OR referrer like '%google_ads%' OR referrer like '%google%')
+and referrer not like '%youtube%'
+and referrer not like '%google.com%'
+and referrer not like '%google.co.in%'
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'Tick_Tok_Job_Posts' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of, date(jp.timestamp) as Created_at,
+count(distinct(jp.job_post_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (jp.referrer like '%pg_emp_tt%')
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'Overall_New_Users' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(ed.timestamp) as Created_at,
+count(distinct(jp.user_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (ed.timestamp) between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'FB_New_Users' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(ed.timestamp) as Created_at,
+count(distinct(jp.user_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (referrer like '%fb %' OR referrer like '%=fb%' OR referrer like '%=Fb%')
+and (ed.timestamp) between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'Google_New_Users' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(ed.timestamp) as Created_at,
+count(distinct(jp.user_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (referrer like '%SEM%' OR referrer like '%gclid%' OR referrer like '%Google%' OR referrer like '%google_ads%' OR referrer like '%google%')
+and referrer not like '%youtube%'
+and referrer not like '%google.com%'
+and referrer not like '%google.co.in%'
+and (ed.timestamp) between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+union
+
+select 'Tick_Tok_New_Users' as Data_cut,case
+    when jp.city = 'mumbai' then 'Mumbai'
+    when jp.city = 'delhi' then 'Delhi'
+    when jp.city = 'pune' then 'Pune'
+    when jp.city = 'bengaluru' then 'Bengaluru'
+    when jp.city = 'hyderabad' then 'Hyderabad'
+    when jp.city = 'ahmedabad' then 'Ahmedabad'
+    when jp.city = 'kolkata' then 'Kolkata'
+    when jp.city = 'chennai' then 'Chennai'
+    when jp.city = 'lucknow' then 'Lucknow'
+    when jp.city = 'jaipur' then 'Jaipur'
+    when jp.city = 'surat' then 'Surat'
+    when jp.city = 'indore' then 'Indore'
+    when jp.city = 'nagpur' then 'Nagpur'
+    when jp.city = 'chandigarh' then 'Chandigarh'
+    when jp.city = 'vadodara' then 'Vadodara'
+    when jp.city = 'patna' then 'Patna'
+    when jp.city = 'bhopal' then 'Bhopal'
+    when jp.city = 'nashik' then 'Nashik'
+    when jp.city = 'kanpur' then 'Kanpur'
+    when jp.city = 'varanasi' then 'Varanasi'
+    when jp.city = 'bhubaneswar' then 'Bhubaneswar'
+    when jp.city = 'dehradun' then 'Dehradun'
+    when jp.city = 'coimbatore' then 'Coimbatore'
+    when jp.city = 'ludhiana' then 'Ludhiana'
+    when jp.city = 'mohali' then 'Mohali'
+    else 'other'
+end City_, 'Today'  as Data_As_Of ,date(ed.timestamp) as Created_at,
+count(distinct(jp.user_id)) as Value_
+from wi_employer_registration as ed
+join wi_job_posting as jp on ed.employer_id = jp.user_id
+where (jp.referrer like '%pg_emp_tt%')
+and (ed.timestamp) between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+and (jp.timestamp)  between date(getdate()) ||' 00:00:00' and date(getdate()) ||' 23:59:59'
+group by Data_cut,City_,Data_As_Of,Created_at
+
+
+"""
+print("<-------------------Start--------------->")
+x1.execute(sql1)
+conn2.commit()
+#print("The Data Type is :" , type(data1))
+df = DataFrame(x1.fetchall())
+df.columns = ['data_cut','city_','data_as_of','created_at','value_']
+df.to_csv('Output_Final_MMDB.csv', index=False, header=True)
+print("Data Fetched From SQL:")
+#print(df)
+from datetime import datetime, date, time
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('/home/ubuntu/da/Master_Marketing_DashBoard/workindia-dashboards-721a8c43109c.json', scope)
+client = gspread.authorize(creds)
+print("Connected to Google sheets")
+sheet = client.open("Marketing_MasterDashBoard_v1")
+worksheet = sheet.worksheet('_New_Users_PVT_Raw_Data')
+
+########################################## Read The master Dump File
+df2 = pd.read_csv("Output_Final_MMDB.csv")
+df = pd.read_csv("/home/ubuntu/da/Master_Marketing_DashBoard/The_Data_Dump_MMDB.csv")
+print("Master & Output, both File Read")
+########################################## Read The master Dump File
+
+today = datetime.combine(date.today(), time())
+print("Before changing the formate")
+#print(df)
+
+print("Converting string dates from csv to datetime for comparison")
+try:
+    df['created_at'] = pd.to_datetime(df['created_at'], format="%d-%m-%Y")
+except ValueError:
+    df['created_at'] = pd.to_datetime(df['created_at'])
+
+print("After changing the formate")
+#print(df)
+
+df_old_date =  df[df['created_at'] < today]
+
+df_old_date['created_at'] = df_old_date['created_at'].dt.strftime('%Y-%m-%d')
+
+print("Marketing Dump, after date format adjustment")
+out = df_old_date.append(df2)
+#out = pd.concat([df_old_date, df2], sort=False)
+out.to_csv("appended_data_MMDB.csv", index=False)
+#print(out)
+
+print("Index Removed")
+data_list = []
+print("Data List Created")
+df3 = pd.read_csv("appended_data_MMDB.csv")
+headers = list(out)
+data_with_headers = [headers] + out.values.tolist()
+#print(data_with_headers[:10])
+df3.to_csv("/home/ubuntu/da/Master_Marketing_DashBoard/The_Data_Dump_MMDB.csv", index = False)
+worksheet.clear()
+sheet.values_append("'_New_Users_PVT_Raw_Data'!A:E", {'valueInputOption': 'USER_ENTERED'}, {"values": data_with_headers})
+sheet.values_append("'_New_Users_PVT_Raw_Data'!K:K", {'valueInputOption': 'USER_ENTERED'}, {"values": [[str(datetime.now())]]})
+os.remove("appended_data_MMDB.csv")
+os.remove("Output_Final_MMDB.csv")
+print(today)
+print("Data Pasted")
+print("<-------------------End--------------->")
+
+
+# In[ ]:
+
+
+
+
+
+# In[32]:
+
+
+
+
+
+# In[34]:
+
+
+
+
